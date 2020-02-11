8b5ba898d81e91251b87b4a055d34e407ac32c64
Luke-F-Harris <luke-f-harris@users.noreply.github.com>
2020-02-11 09:51:58 -0500

    retry

---
+++ 2250 Project/Assets/Scenes/Scripts/ScriptableObjects/ExpBar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ExpBar : ScriptableObject
+{
+    public int experience = 0;
+    private int [] _thresholds = {10,20,30,40,50};
+
+    public void gainExperience(int experience){
+        if((this.experience+=experience)>_thresholds[PlayerMovement.instance.GetLevel()]){
+            
+        }
+    }
+}
