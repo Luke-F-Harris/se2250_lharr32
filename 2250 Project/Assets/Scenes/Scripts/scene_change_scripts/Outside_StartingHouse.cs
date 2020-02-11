52b14049b597ee0ae51cfd5b9b3d5c7c7e9b1d46
Luke-F-Harris <luke-f-harris@users.noreply.github.com>
2020-02-11 00:22:28 -0500

    First commit

---
+++ 2250 Project/Assets/Scenes/Scripts/scene_change_scripts/Outside_StartingHouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Outside_StartingHouse : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("DownStairsOfStartingRoom");
+
+        }
+    }
+}
