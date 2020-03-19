c3109f31a3d8a9967e95dbca6a0a0627ecae0f78
Luke-F-Harris <luke-f-harris@users.noreply.github.com>
2020-03-19 19:35:27 -0400

    added customization

---
+++ 2250 Project/Assets/Scenes/Scripts/customization.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class customization : MonoBehaviour
+{
+
+
+    private bool _playerInZone;
+    public GameObject pressX;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressX.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_playerInZone)
+        {
+            pressX.SetActive(true);
+            FollowObject(target);
+            if (Input.GetKey("x"))
+            {
+                Debug.Log("You just changed clothes!");
+            }
+        }
+        else
+        {
+            pressX.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _playerInZone = true;
+        }
+    }
+    private void FollowObject(GameObject other)
+    {
+        pressX.transform.position = Camera.main.WorldToScreenPoint(other.transform.position) + new Vector3(0, 25, 0);
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _playerInZone = false;
+        }
+    }
+}
