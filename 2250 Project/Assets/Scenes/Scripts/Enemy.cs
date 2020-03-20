44f5348aa98bdd2895acd31faf6b566cd3c9fa28
Luke-F-Harris <luke-f-harris@users.noreply.github.com>
2020-03-20 00:46:49 -0400

    sumbit\

--- 2250 Project/Assets/Scenes/Scripts/Enemy.cs
+++ 2250 Project/Assets/Scenes/Scripts/Enemy.cs
@@ -33,23 +33,24 @@ public class Enemy : MonoBehaviour
 
         }
     }
-}
 
-void Attack()
-{
-    BattleItemScript item = gameObject.GetComponent<BattleItemScript>();
-    int rangeX = item.item.rangeX, rangeZ = item.item.rangeZ;
 
-    GameObject attack = Instantiate(AttackHitBox, gameObject.transform.position + gameObject.transform.forward, Quaternion.identity); //spawns a boxcollider in the attack range
-    attack.GetComponent<AttackCollider>().damage = item.item.damage;
-    attack.GetComponent<BoxCollider2D>().size = new Vector3(rangeX, 1, rangeZ);
-    attack.tag = gameObject.tag == "Player" ? "PlayerAttack" : "EnemyAttack";
-}
+    // void Attack()
+    // {
+    //     BattleItemScript item = gameObject.GetComponent<BattleItemScript>();
+    //     int rangeX = item.item.rangeX, rangeZ = item.item.rangeZ;
 
-void OnCollisionEnter2D(Collision2D collision)
-{
-    if (collision.gameObject.tag == "EnemyAttack")
+    //     GameObject attack = Instantiate(AttackHitBox, gameObject.transform.position + gameObject.transform.forward, Quaternion.identity); //spawns a boxcollider in the attack range
+    //     attack.GetComponent<AttackCollider>().damage = item.item.damage;
+    //     attack.GetComponent<BoxCollider2D>().size = new Vector3(rangeX, 1, rangeZ);
+    //     attack.tag = gameObject.tag == "Player" ? "PlayerAttack" : "EnemyAttack";
+    // }
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        gameObject.GetComponent<Health>().Damage(collision.gameObject.GetComponent<AttackCollider>().damage);
+        if (collision.gameObject.tag == "EnemyAttack")
+        {
+            gameObject.GetComponent<Health>().Damage(collision.gameObject.GetComponent<AttackCollider>().damage);
+        }
     }
 }
