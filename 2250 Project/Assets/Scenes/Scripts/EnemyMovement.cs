5c15539295750ad63da0cae733d65efac82ed088
andrewharrop <60928763+andrewharrop@users.noreply.github.com>
2020-03-19 20:04:39 -0400

    good

---
+++ 2250 Project/Assets/Scenes/Scripts/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyMovement : MonoBehaviour
+{
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2250 Project/Assets/Scenes/Scripts/Health.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    //can be used for many entities
+    public float health = 100f;
+    private bool dead = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (health < 0)
+        {
+            dead = true; 
+        }
+
+        
+    }
+    void setDead(bool val)
+    {
+        dead = val;
+    }
+    bool getDead()
+    {
+        return dead;
+    }
+
+}
